f2f4c1fb1541d2b95691799569e07f7b97694b80
Yiorgis Gozadinos <ggozad@jarn.com>
2011-09-24 19:38:14 +0200

    Add the xmpp bundle to the CMSUI bundles

--- jarn/xmpp/core/setuphandlers.py
+++ jarn/xmpp/core/setuphandlers.py
@@ -1,7 +1,15 @@
+from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
-
+from Products.ResourceRegistries.interfaces.settings import IResourceRegistriesSettings
 
 def setupCMSUI(context):
     if context.readDataFile('cmsui.txt') is None:
         return
-    portal = context.getSite()
+    registry = getUtility(IRegistry)
+    rr_settings = registry.forInterface(IResourceRegistriesSettings)
+    bundles = getattr(rr_settings, 'resourceBundlesForThemes', None)
+    if bundles is None:
+        return
+    if 'cmsui' not in bundles:
+        return
+    bundles['cmsui'] = bundles['cmsui'] + ['xmpp']
